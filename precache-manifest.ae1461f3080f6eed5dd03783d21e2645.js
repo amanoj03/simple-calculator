19e94e29ef7ffbdeac6515a8941af24f64d39924
amanoj03 <amanoj271@gmail.com>
2020-09-01 13:27:52 +0530

    Updates

---
+++ precache-manifest.ae1461f3080f6eed5dd03783d21e2645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b12975d5053fe086505d60cf2f15643f",
+    "url": "/simple-calculator/index.html"
+  },
+  {
+    "revision": "0d1c684b049fa02a2d3a",
+    "url": "/simple-calculator/static/css/main.1c15876f.chunk.css"
+  },
+  {
+    "revision": "abd4faa599eae2b7b11c",
+    "url": "/simple-calculator/static/js/2.9337a47f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-calculator/static/js/2.9337a47f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d1c684b049fa02a2d3a",
+    "url": "/simple-calculator/static/js/main.460025e3.chunk.js"
+  },
+  {
+    "revision": "83d88178bf22fe801a5e",
+    "url": "/simple-calculator/static/js/runtime-main.2dce0495.js"
+  }
+]);
